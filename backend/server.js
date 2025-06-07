7db1ae00c8fb311690222137a1c3086b10360b9b
nitishk99 <112569135+nitishk99@users.noreply.github.com>
2025-06-07 22:32:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/auth", userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/mail', mailRouter);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
